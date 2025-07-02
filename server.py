2c0204f14fbaadd489e47c17a697b0ba10cf4209
Ted Vaala <ted.vaala@gmail.com>
2025-07-02 16:03:43 -0400

    Task 6 complete

--- server.py
+++ server.py
@@ -1,12 +1,29 @@
-# app.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template, url_for, redirect
+import requests
+from EmotionDetection import emotion_detection as ED
 
 app = Flask(__name__)
 
-@app.route('/quote')
-def get_quote():
-    quote = random.choice(quotes)
-    return jsonify({'quote': quote})
+@app.route('/hello')
+def hello_world():
+    return "Hello World!"
+
+
+@app.route('/emotionDetector', methods=['GET'])
+def get_emotion():
+    text_to_analyze = request.args.get('textToAnalyze', '')
+    if not text_to_analyze:
+        return render_template('index.html')
+
+    analysis = ED.emotion_detector(text_to_analyze)
+    output = f"For the given statement, the system response is "
+    output += f"'anger': {analysis['anger']}, "
+    output += f"'disgust': {analysis['disgust']}, "
+    output += f"'fear': {analysis['fear']}, "
+    output += f"'joy': {analysis['joy']}, "
+    output += f"and 'sadness': {analysis['sadness']}."
+    output += f" The dominant emotion is {analysis['dominant_emotion']}."    
+    return output
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
