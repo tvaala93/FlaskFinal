1dbbf2dae3c0a94788e7cb506e2854439073f260
Ted Vaala <ted.vaala@gmail.com>
2025-07-02 16:29:21 -0400

    Project complete

--- server.py
+++ server.py
@@ -1,30 +1,30 @@
-from flask import Flask, jsonify, request, render_template, url_for, redirect
-import requests
+'''
+server.py - This module handles the server-side operations using Flask
+'''
+from flask import Flask, request
 from EmotionDetection import emotion_detection as ED
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello_world():
-    return "Hello World!"
-
-
 @app.route('/emotionDetector', methods=['GET'])
 def get_emotion():
+    '''
+    This function gets a user input from the web and routes it to the emotion detector.
+    The response is then returned to the webpage.
+    '''
+
     text_to_analyze = request.args.get('textToAnalyze', '')
-    #if not text_to_analyze:
-    #    return render_template('index.html')
 
     analysis = ED.emotion_detector(text_to_analyze)
     if analysis['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    output = f"For the given statement, the system response is "
+    output = "For the given statement, the system response is "
     output += f"'anger': {analysis['anger']}, "
     output += f"'disgust': {analysis['disgust']}, "
     output += f"'fear': {analysis['fear']}, "
     output += f"'joy': {analysis['joy']}, "
     output += f"and 'sadness': {analysis['sadness']}."
-    output += f" The dominant emotion is {analysis['dominant_emotion']}."    
+    output += f" The dominant emotion is {analysis['dominant_emotion']}."
     return output
 
 if __name__ == '__main__':
