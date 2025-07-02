ce87e599c215cdba7c0290d21a5bf3a814c2c55a
Ted Vaala <ted.vaala@gmail.com>
2025-07-02 11:24:24 -0400

    Task3 complete

--- emotion_detection.py
+++ emotion_detection.py
@@ -4,6 +4,7 @@ import requests
 
 URL= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 Headers= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+TEST_STRING = "I love this technology."
 
 def emotion_detector(text_to_analyze:str):
     payload = { "raw_document": { "text": text_to_analyze}}
@@ -16,5 +17,7 @@ def emotion_detector(text_to_analyze:str):
 
     # Check for errors
     if response.status_code == 200:
-        data = response.json()
-        print(data)
+        data = response.json()["emotionPredictions"][0]["emotion"]
+        dominant_emotion = max(data, key=data.get)        
+        data['dominant_emotion'] = dominant_emotion
+        print(json.dumps(data,indent=4))
